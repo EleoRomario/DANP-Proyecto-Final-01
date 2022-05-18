d50d2a840a7d9a9497ce6ede16c36d0a4dd49369
Rolando <rleonmam@unsa.edu.pe>
2022-05-17 20:16:18 -0500

    Terminado detalle lugar turistico

--- app/src/main/java/com/idnp/danp_proyecto_final/screens/DetalleLugarTuristico.kt
+++ app/src/main/java/com/idnp/danp_proyecto_final/screens/DetalleLugarTuristico.kt
@@ -1,8 +1,11 @@
 package com.idnp.danp_proyecto_final.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
@@ -19,6 +22,11 @@ import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.rememberCameraPositionState
 import com.idnp.danp_proyecto_final.R
 import com.idnp.danp_proyecto_final.ui.theme.TextAlt
 
@@ -36,7 +44,9 @@ fun LugarTuristico(){
 @Composable
 fun LugarBodyContent(){
 
-    Column() {
+    val scrollState = rememberScrollState()
+
+    Column(modifier = Modifier.verticalScroll(scrollState)) {
         Image(painter = painterResource(id = R.drawable.bosqueneblinacarpish), contentDescription = "",
             contentScale  = ContentScale.Crop,
             modifier= Modifier
@@ -47,18 +57,11 @@ fun LugarBodyContent(){
         infoLugarTuristico()
     }
 
-
-
-
-/*    Column {
-        MyImage()
-        MyText("Titulo", "detalle")
-        MyGoogleMap()
-      }*/
 }
 
 @Composable
 fun infoLugarTuristico(){
+
     Column(
         modifier = Modifier.padding(20.dp)
     ) {
@@ -88,13 +91,35 @@ fun infoLugarTuristico(){
         Text("Descripcion", fontSize = 16.sp)
         Spacer(modifier = Modifier.height(8.dp))
         Text(text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pretium erat quis luctus vestibulum. In arcu mauris, porttitor sit amet gravida non, pulvinar vitae purus.", fontSize = 14.sp, color = TextAlt)
-        mapLugarTuristico()
+        Spacer(modifier = Modifier.height(10.dp))
+        Row(modifier = Modifier.height(400.dp)){
+            mapLugarTuristico()
+        }
     }
 }
 
 @Composable
 fun mapLugarTuristico(){
-    //openstreetmap
+    val marker = LatLng(-9.5158261,-76.0369947)
+    val cameraPositionState = rememberCameraPositionState{
+        position = CameraPosition.fromLatLngZoom(marker, 10f)
+    }
+
+    Box () {
+        GoogleMap(
+            //modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(0.dp)
+                .height(400.dp)
+                .clip(RoundedCornerShape(20.dp, 20.dp, 20.dp, 20.dp)),
+            cameraPositionState = cameraPositionState
+        ){
+            Marker(position = marker, title = "Lugar", snippet = "Bonito lugar")
+        }
+    }
+
+
 }
 
 //@Preview(showSystemUi = true)
@@ -102,6 +127,5 @@ fun mapLugarTuristico(){
 @Composable
 fun LugaresDefaultPreview() {
     LugarTuristico()
-    //LugarBodyContent()
-    //MyText("palabra","texto")
+
 }
